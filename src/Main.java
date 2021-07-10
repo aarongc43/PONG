dac7951d9a14fc3be4811a5f57337cd14c4795be
Aaron Castillo <aarongc43@gmail.com>
2021-07-10 14:16:53 -0500

    new file:   Main.class 	modified:   Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import javafx.scene.Group;
 import javafx.scene.shape.Rectangle;
 import java.util.Random;
 import javafx.animation.KeyFrame;
@@ -38,27 +40,38 @@ public class Main extends Application {
     private double playerTwoXPos = width - (PLAYER_WIDTH + 15);
     private double playerTwoYPos = height / 2; 
 
+    private int scoreP1;
+    private int scoreP2;
+
     private boolean gameStarted = false;
 
     public void start(Stage stage) throws Exception {
+
+        Group root = new Group();
+        Scene theScene = new Scene(root);
+        Canvas canvas = new Canvas(width, height);
+        stage.setScene(new Scene(new StackPane(canvas)));
 
         Image icon = new Image("pong_logo.jpeg");
         stage.setTitle("PONG");
         stage.getIcons().add(icon);
 
-        Canvas canvas = new Canvas(width, height);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         // Animation
-        Timeline tl = new Timeline(new KeyFrame(Duration.millis(10), e -> run(gc)));
+        Timeline tl = new Timeline(new KeyFrame(Duration.millis(16), e -> run(gc)));
         tl.setCycleCount(Timeline.INDEFINITE);
 
         canvas.setFocusTraversable(true);
+
         canvas.setOnKeyPressed(e -> {
             keyPressed(e);
         });
 
-        stage.setScene(new Scene(new StackPane(canvas)));
+        canvas.setOnKeyReleased(e -> {
+            keyPressed(e);
+        });
+
         stage.show();
         tl.play();
     }
@@ -88,29 +101,53 @@ public class Main extends Application {
             ballYSpeed = new Random().nextInt(2) == 0 ? 1: -1;
         }
 
+        // Keeps the ball in the court
+        if (ballYPos > height || ballYPos < 0) {
+            ballYSpeed *=-1;
+        }
+
+        // TODO: playerTwo gets a point
+        if (ballXPos < playerOneXPos + (PLAYER_WIDTH - 15)) {
+            scoreP2++;
+            gameStarted = false;
+        }
+
+        // playerOne gets a point
+        if (ballXPos > playerTwoXPos + (PLAYER_WIDTH + 15)) {
+            scoreP1++;
+            gameStarted = false;
+        }
+
+        // increase speed after the ball hits the player
+        if (((ballXPos + BALL_RADIUS > playerTwoXPos) && ballYPos >= playerTwoYPos && ballYPos <= playerTwoYPos + PLAYER_HEIGHT)
+                || ((ballXPos < playerOneXPos + PLAYER_WIDTH) && ballYPos >= playerOneYPos && ballYPos <= playerOneYPos + PLAYER_HEIGHT)) {
+            ballYSpeed += 1 * Math.cos(ballYSpeed);
+            ballXSpeed += 1 * Math.sin(ballXSpeed);
+            ballXSpeed *= -1;
+            ballYSpeed *= -1;
+                }
+
+        // TODO: display score
+        gc.fillText(scoreP1 + "\t" + scoreP2, width / 2, 100);
+
         gc.fillRect(playerTwoXPos, playerTwoYPos, PLAYER_WIDTH, PLAYER_HEIGHT);
         gc.fillRect(playerOneXPos, playerOneYPos, PLAYER_WIDTH, PLAYER_HEIGHT);
     }
 
     public void keyPressed (KeyEvent e) {
-        if (e.getCode() == KeyCode.SPACE) {
-            gameStarted = true;
+        ArrayList<String> input = new ArrayList<>();
+
+        String code = e.getCode().toString();
+
+        if (!input.contains(code)) {
+            input.add(code);
+            if (input.get(0).equals("SPACE")) {
+                gameStarted = true;
+            }
         }
 
-        if (e.getCode() == KeyCode.W) {
-            playerOneYPos += -50;
-        }
-
-        if (e.getCode() == KeyCode.S) {
-            playerOneYPos += 50;
-        }
-
-        if (e.getCode() == KeyCode.UP) {
-            playerTwoYPos += -50;
-        }
-
-        if (e.getCode() == KeyCode.DOWN) {
-            playerTwoYPos += 50;
+        if (!input.contains(code)) {
+            input.add(code);
         }
     }
 
